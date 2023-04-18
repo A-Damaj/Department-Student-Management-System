9f25113626f7d8dd8dae1ffae4fbdeb6cc77a734
A-Damaj <106034915+A-Damaj@users.noreply.github.com>
2023-04-19 00:54:43 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
-
+# Define variables
+student_folder="CurrentStudents"
+backup_folder="CurrentStudentsBackup"
+date=$(date +%d_%B_%y)
 
 while true; do
-  # Prompt the user to enter a menu choice
-  read -p "Enter choice: " choice
-
+  echo ""
+  read -p "Please choose an option (1-6): " choice
   case "$choice" in
-  1) # List all students
-    list_all
-    ;;
-  2) # Display student info
-    display_info
-    ;;
-  3) # Count students by major
-    count_students
-    ;;
-  4) # Delete a student file
-    delete_student
-    ;;
-  5) # Create a backup of the data directory
-    backup
+    1) list_all ;;
+    2) display_info ;;
+    3) count_students ;;
+    4) delete_student ;;
+    5) backup ;;
+    6) echo "Exiting program." ; break ;;
+    *) echo "Invalid choice. Please choose an option from 1 to 6." ;;
+  esac
+done
+
+
+
+
+
+
+
+
+
+
