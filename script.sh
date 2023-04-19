251d1e6e39b35b5b2e1dde1c399d0e42549dc39a
A-Damaj <106034915+A-Damaj@users.noreply.github.com>
2023-04-19 18:32:00 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,21 +1,46 @@
 #!/bin/bash
 
-
 # Define variables
-student_folder="CurrentStudents"  # directory where student files are stored
-backup_folder="CurrentStudentsBackup"  # name of backup folder
-date=$(date +%d_%B_%y)  # variable for storing the current date in the format day_FullMonthName_Last2DigitsYear
+student_folder="CurrentStudents"   # directory where student files are stored
+backup_folder="CurrentStudentsBackup"   # name of backup folder
+date=$(date +%d_%B_%y)   # variable for storing the current date in the format day_FullMonthName_Last2DigitsYear
 
-while true; do  # start an infinite loop
-    echo ""  # print an empty line
-    read -p "Please choose an option (1-6): " choice  # prompt user to enter a choice from the menu
-    case "$choice" in  # use case statement to execute a specific function based on user's choice
-        1) list_all ;;  # if choice is 1, execute list_all function
-        2) display_info ;;  # if choice is 2, execute display_info function
-        3) count_students ;;  # if choice is 3, execute count_students function
-        4) delete_student ;;  # if choice is 4, execute delete_student function
-        5) backup ;;  # if choice is 5, execute backup function
-        6) echo "Exiting program." ; break ;;  # if choice is 6, print a message and break out of the loop to exit the program
-        *) echo "Invalid choice. Please choose an option from 1 to 6." ;;  # if choice is not between 1-6, print an error message
-    esac  # end the case statement
-done  # end the infinite loop
+while true; do   # start an infinite loop
+    echo ""   # print an empty line
+
+    # Print the menu options and prompt user to choose an option
+    # The "select" command displays a numbered list of options and takes care of user input validation
+    PS3="Please choose an option (1-6): "   # Set the PS3 environment variable prompt to display a message for selecting an option from the menu
+    options=("List all students" "Display student information" "Count students" "Delete a student" "Backup student files" "Exit")
+    select choice in "${options[@]}"; do
+        case "$choice" in
+            "List all students")
+                list_all   # if choice is 1, execute list_all function
+                break
+                ;;
+            "Display student information")
+                display_info   # if choice is 2, execute display_info function
+                break
+                ;;
+            "Count students")
+                count_students   # if choice is 3, execute count_students function
+                break
+                ;;
+            "Delete a student")
+                delete_student   # if choice is 4, execute delete_student function
+                break
+                ;;
+            "Backup student files")
+                backup   # if choice is 5, execute backup function
+                break
+                ;;
+            "Exit")
+                echo "Exiting program."
+                exit 0   # if choice is 6, print a message and exit the program with status code 0 (success)
+                ;;
+            *)   # if choice is not between 1-6, print an error message and prompt user to choose again
+                echo "Invalid choice. Please choose an option from 1 to 6."
+                ;;
+        esac
+    done
+done
