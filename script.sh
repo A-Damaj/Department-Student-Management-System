9138b1a52f5101d6319732aaadf8f44bc242c5f1
A-Damaj <106034915+A-Damaj@users.noreply.github.com>
2023-04-19 18:02:53 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
+
+
 # Define variables
-student_folder="CurrentStudents"
-backup_folder="CurrentStudentsBackup"
-date=$(date +%d_%B_%y)
+student_folder="CurrentStudents"  # directory where student files are stored
+backup_folder="CurrentStudentsBackup"  # name of backup folder
+date=$(date +%d_%B_%y)  # variable for storing the current date in the format day_FullMonthName_Last2DigitsYear
 
-while true; do
-  echo ""
-  read -p "Please choose an option (1-6): " choice
-  case "$choice" in
-    1) list_all ;;
-    2) display_info ;;
-    3) count_students ;;
-    4) delete_student ;;
-    5) backup ;;
-    6) echo "Exiting program." ; break ;;
-    *) echo "Invalid choice. Please choose an option from 1 to 6." ;;
-  esac
-done
-
-
-
-
-
-
-
-
-
-
+while true; do  # start an infinite loop
+    echo ""  # print an empty line
+    read -p "Please choose an option (1-6): " choice  # prompt user to enter a choice from the menu
+    case "$choice" in  # use case statement to execute a specific function based on user's choice
+        1) list_all ;;  # if choice is 1, execute list_all function
+        2) display_info ;;  # if choice is 2, execute display_info function
+        3) count_students ;;  # if choice is 3, execute count_students function
+        4) delete_student ;;  # if choice is 4, execute delete_student function
+        5) backup ;;  # if choice is 5, execute backup function
+        6) echo "Exiting program." ; break ;;  # if choice is 6, print a message and break out of the loop to exit the program
+        *) echo "Invalid choice. Please choose an option from 1 to 6." ;;  # if choice is not between 1-6, print an error message
+    esac  # end the case statement
+done  # end the infinite loop
